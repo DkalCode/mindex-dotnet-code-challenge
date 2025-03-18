1253ea92304e277dc630a84b12be23ad93690f79
DkalCode <dkalmc@gmail.com>
2025-03-18 15:54:20 -0400

    fix: fixed null return issue when trying to access an Employee's DirectReports

--- CodeChallenge/Repositories/EmployeeRespository.cs
+++ CodeChallenge/Repositories/EmployeeRespository.cs
@@ -29,7 +29,7 @@ namespace CodeChallenge.Repositories
 
         public Employee GetById(string id)
         {
-            return _employeeContext.Employees.SingleOrDefault(e => e.EmployeeId == id);
+            return _employeeContext.Employees.Include(r => r.DirectReports).SingleOrDefault(e => e.EmployeeId == id);
         }
 
         public Task SaveAsync()
