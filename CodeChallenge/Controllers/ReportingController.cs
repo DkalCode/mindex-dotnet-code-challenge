b3f648d2304768d054af608946cd54b7728c1dd5
DkalCode <dkalmc@gmail.com>
2025-03-18 15:51:26 -0400

    feat: Implemented ReportingController to define and handle the new endpoint (api/reporting).

---
+++ CodeChallenge/Controllers/ReportingController.cs
@@ -0,0 +1,40 @@
+﻿using CodeChallenge.Models;
+using CodeChallenge.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CodeChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/reporting")]
+    public class ReportingController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IReportingService _reportingService;
+
+        public ReportingController(ILogger<EmployeeController> logger, IReportingService reportingService)
+        {
+            _logger = logger;
+            _reportingService = reportingService;
+        }
+
+        /*
+         * HTTP Method: GET 
+         * URL: /api/reporting/{id}
+         * RESPONSE: ReportingStructure
+         */
+        [HttpGet("{id}", Name = "getReportingByEmployeeId")]
+        public IActionResult GetReportingByEmployeeId(string id)
+        {
+            _logger.LogDebug($"Received reporting get request for '{id}'");
+
+            ReportingStructure reporting = _reportingService.GetById(id);
+
+            if (reporting == null)
+                return NotFound();
+
+            return Ok(reporting);
+        }
+    }
+}
