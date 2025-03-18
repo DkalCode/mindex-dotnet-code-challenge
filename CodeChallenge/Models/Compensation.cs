e5a644f5de08ae22ee00464f725f46e765c415de
DkalCode <dkalmc@gmail.com>
2025-03-18 18:21:41 -0400

    fix: Added CompensationId due to primary key requirement on DbSet/Context

--- CodeChallenge/Models/Compensation.cs
+++ CodeChallenge/Models/Compensation.cs
@@ -4,7 +4,8 @@ namespace CodeChallenge.Models
 {
     public class Compensation
     {
-        public Employee employee { get; set; }
+        public string CompensationId { get; set; }
+        public Employee Employee { get; set; }
         public double Salary { get; set; }
         public DateTime EffectiveDate { get; set; }
     }
